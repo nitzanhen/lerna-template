9a2ae2f8f2a6dafce869e674578b8c7d3eeb025c
Nitzan@agamim <58311807+NitzanHen@users.noreply.github.com>
2021-05-30 11:38:08 +0300

    Fix vite failing to resolve common

--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -1,7 +1,13 @@
+import path from 'path';
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [reactRefresh()],
+  resolve: {
+    alias: {
+      '@template/common': path.join(__dirname, '..', 'common', 'src', 'index.ts')
+    }
+  }
 })
